07a11a091a1a76faf0c303dc5a23d596d447429d
ayush_kr <hellotoayush@gmail.com>
2023-10-18 13:26:21 +0530

    ALMOST completed

--- src/App.js
+++ src/App.js
@@ -1,49 +1,133 @@
+import { useEffect, useRef, useState } from 'react';
 import { Box, Button, Container, HStack, Input, VStack } from '@chakra-ui/react'
 import Message from './components/Message';
 
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth'
+import { app } from './firebase'
+
+// ----------------------------------
+import { getFirestore, addDoc, collection, orderBy, serverTimestamp, onSnapshot, query } from 'firebase/firestore'
+
+
+// -----------------------------------
+const auth = getAuth(app);
+
+const loginHandler = () => {
+  const provider = new GoogleAuthProvider();
+
+  signInWithPopup(auth, provider);
+}
+
+const db = getFirestore(app);
+
 function App() {
+  const divScrollToLatestMsg = useRef(null); // element.scrollIntoView() Method
+  const [user, setUser] = useState(false);
+  console.log(user);
+
+  const [message, setMessage] = useState("");
+  const [conversation, setConversation] = useState([])
+  
+  
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    
+    try {
+      setMessage(""); // after msg is sent make it empty
+      
+      await addDoc(collection(db, "Messages"), {
+        text: message,
+        uid: user.uid,
+        uri: user.photoURL,
+        createdAt: serverTimestamp(),
+      });
+    
+      divScrollToLatestMsg.current.scrollIntoView({ behavior: 'smooth' });
+      
+    } catch (error) {
+      alert(error)
+    }
+  }
+  
+  // -----------------------
+  useEffect(() => {
+    const myQuery = query(collection(db, 'Messages'), orderBy('createdAt', 'asc'));
+    const unsubscribe = onAuthStateChanged(auth, (data) => {
+      // console.log(data);
+      setUser(data);
+    });
+    
+    // const unsubscribeForMessage = onSnapshot(collection(db, "Messages"), (snap) => {
+      const unsubscribeForMessage = onSnapshot(myQuery, (snap) => {
+      setConversation(
+        snap.docs.map((item) => {
+          // const data = item.data;
+          const id = item.id; // all items id from database is retrieved          
+
+          // return {a: "abcd"};
+          // return {id, a: "abcd"};
+          return { id, ...item.data() };
+        }));
+    })
+    
+    return () => {
+      unsubscribe();
+      unsubscribeForMessage();
+    }
+    
+  }, [])
+  // }, [user])
+
+  const logoutHandler = () => {
+    signOut(auth)
+  }
+
+
+
   return (
     <>
-      <Box bg={'red.100'} >
-        <Container h={"100vh"} bg={'telegram.100'} className=''  >
-          <VStack h='full' paddingY={'4'} >
-            <Button colorScheme={'red'} w={'full'} >Log Out</Button>
+      {
+        user ? (<Box bg={'red.100'} >
+          <Container h={"100vh"} bg={'telegram.100'} className=''  >
+            <VStack h='full' paddingY={'4'} >
+              <Button onClick={logoutHandler} colorScheme={'red'} w={'full'} >Log Out</Button>
 
-            {/* Conversation Design - Middle Main Part */}
-            {/* <VStack height="lg" w={'full'} bg={'purple.200'} > */}
-            <VStack w={'full'} bg={'purple.200'} className='h-[550px] overflow-y-auto' >
-              <Message text={"User Name"} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} user={'me'} />
-              <Message text={"User Name"} />
+              {/* Conversation Design - Middle Main Part */}
+              {/* <VStack height="lg" w={'full'} bg={'purple.200'} > */}
+              <VStack w={'full'} bg={'purple.200'} className='h-[550px] overflow-y-auto' >
+                {/* <Message text={"Sample Msg"} user={'me'} /> */}
+                {/* <Message text={"Sample Msg"} /> */}
+
+                {
+                  conversation.map((item) => (
+                    <Message
+                      key={item.id}
+                      user={item.uid === user.uid ? "me" : "other"}
+                      text={item.text}
+                      uri={item.uri}
+                    />
+                  ))
+                }
+
+                <div ref={divScrollToLatestMsg} ></div>
+              </VStack>
+
+              {/* Footer Send Button */}
+              <form onSubmit={submitHandler} action="" className='w-full' >
+                <HStack>
+                  <Input value={message} onChange={(e) => setMessage(e.target.value)} placeholder='Write your message here ...' />
+                  <Button colorScheme={'purple'} type='submit' >Send</Button>
+                </HStack>
+
+              </form>
+
             </VStack>
-
-            {/* Footer Send Button */}
-            <form action="" className='w-full' >
-              <HStack>
-                <Input />
-                <Button colorScheme={'purple'} type='submit' >Send</Button>
-              </HStack>
-
-            </form>
-
-          </VStack>
-        </Container>
-      </Box>
+          </Container>
+        </Box>) :
+          (<VStack h={'100vh'} justifyContent={'center'} >
+            <Button onClick={loginHandler} colorScheme='blue' size={'md'} mb={6}>Login to your account</Button>
+          </VStack>)
+      }
     </>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDw7wh_XaSM_t6TtF8ocpLqNIeAzeUH8BM",
+    authDomain: "chat-app-react-35ddd.firebaseapp.com",
+    projectId: "chat-app-react-35ddd",
+    storageBucket: "chat-app-react-35ddd.appspot.com",
+    messagingSenderId: "701098181151",
+    appId: "1:701098181151:web:5216353c9d5c2a62878b2a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
